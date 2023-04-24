cb35040e7a7ffc48035a16d9d4aa60d2f0536e3a
amberhuang0116 <58518314+amberhuang0116@users.noreply.github.com>
2023-04-24 16:49:24 -0700

    Added R File

---
+++ Market Basket.R
@@ -0,0 +1,55 @@
+library(readxl)
+library(plyr)
+library(dplyr)
+library(knitr)
+library(arules)
+library(arulesViz)
+library(tidyverse)
+library(ggplot2)
+library(magrittr)
+
+#load data
+items <- read_excel('/Users/vinay/Downloads/archive/Assignment-1_Data.xlsx')
+
+#removing rows with no items
+complete_items <- data.frame(items[complete.cases(items),])
+#nrow(complete_items)
+#nrow(items)
+
+#combining products with same bill no. and date
+transac_data <- ddply(complete_items,c("BillNo","Date"),function(df1)paste(df1$Itemname,collapse= ","))
+transac_data$BillNo <- NULL
+transac_data$Date <- NULL
+colnames(transac_data) <- c("items")
+
+#to csv
+write.csv(transac_data, "/Users/vinay/Downloads/archive/itemlist_data.csv", quote = FALSE, row.names = FALSE)
+
+#Analysing transaction data
+final_transac <- read.transactions("/Users/vinay/Downloads/archive/itemlist_data.csv", format = 'basket', sep =',')
+summary(final_transac)
+
+#Frequency plot
+if(!require("RColorBrewer")){install.packages("RColorBrewer")}
+library(RColorBrewer)
+itemFrequencyPlot(final_transac, topN=20, type ="absolute", col= brewer.pal(8,'Pastel2'), main ="Item Frequency Plot")
+
+#Apriori Rules
+generated.rules <- apriori(final_transac, parameter = list(supp= 0.001, conf= 0.8, maxlen = 10 ))
+generated.rules <- sort(generated.rules, by = 'confidence', decreasing = TRUE)
+summary(generated.rules)
+
+#rules inspection
+inspect(generated.rules[1:5])
+
+#Visualization rules
+Rules <- generated.rules[quality(generated.rules)$confidence>0.6]
+plot(Rules)
+top10Rules <- head(generated.rules, n=10, by="confidence", jitter=0)
+plot(top10Rules)
+
+
+
+
+
+
